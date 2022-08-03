a80de49b6ca8b65e3ed821f29d5fef0c80cb7ca6
Christian <christian.mf22@gmail.com>
2022-08-03 14:36:35 -0300

    feat: Import libs

--- farms/views.py
+++ farms/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
-
-# Create your views here.
+from rest_framework.views import Request, Response
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.authtoken.models import Token
+from farms.models import Farm
