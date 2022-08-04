3ba4772b48800cdc939bfa23af49d2d2552f6142
Christian <christian.mf22@gmail.com>
2022-08-04 20:09:29 -0300

    feat: Created animal model

--- animals/models.py
+++ animals/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+from uuid import uuid4
+
+
+class Animal(models.Model):
+    id = models.UUIDField(primary_key=True, editable=False, default=uuid4)
+    name = models.CharField(max_length=255)
+    weight = models.DecimalField(max_digits=5, decimal_places=2)
+
+    area = models.ForeignKey(
+        "areas.Area", on_delete=models.CASCADE, related_name="animals"
+    )
