bb4b37c68f58b5728083b2f59463707e6a312303
Christian <christian.mf22@gmail.com>
2022-08-03 23:53:40 -0300

    feat: Create area model

--- areas/models.py
+++ areas/models.py
@@ -1,3 +1,10 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from uuid import uuid4
 
-# Create your models here.
+
+class Area(AbstractUser):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    area_name = models.CharField(unique=True, max_length=255)
+    animal_limit = models.IntegerField()
+    gmd = models.DecimalField(max_digits=12, decimal_places=2)
