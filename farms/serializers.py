ef599b8a43375b060ac5106b1f1f46ebbad0ca4f
Christian <christian.mf22@gmail.com>
2022-08-03 22:07:31 -0300

    feat: Create exceptions custom

--- farms/serializers.py
+++ farms/serializers.py
@@ -1,17 +1,29 @@
 from rest_framework import serializers
+from django.db import IntegrityError
 from farms.models import Farm
 
 class FarmSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = Farm
+    class Meta:
+        model = Farm
 
-		fields = ("id", "name",)
+        # fields = ("id", "email", "name",)
+        fields = ("id", "name", "email", "password")
 
-		extra_kwargs = {
-			"id": {"read_only": True},
-			"password": {"write_only": True},
-		}
+        extra_kwargs = {
+            "id": {"read_only": True},
+            "password": {"write_only": True},
+        }
 
-	def create(self, validated_data):
-		print(validated_data)
-		return  Farm.objects.create_user(**validated_data)
+    def create(self, validated_data):
+        try:
+            farm: Farm = self.context["request"].user
+            print("A"*1000)
+            print(farm)
+            # user: Farm = self.context["request"].user
+            validated_data["email"] = validated_data["email"].lower()
+            validated_data["name"] = validated_data["name"].title()
+            # print("A"*1000)
+            return Farm.objects.create_user(**validated_data)
+
+        except:
+            return "Teste"
