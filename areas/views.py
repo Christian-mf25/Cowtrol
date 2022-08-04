c55bd7f4068d78330a568900c7ee1fcfdcf1891b
Christian <christian.mf22@gmail.com>
2022-08-04 10:33:42 -0300

    feat: Create class AreaView

--- areas/views.py
+++ areas/views.py
@@ -1,3 +1,12 @@
+from rest_framework.authentication import TokenAuthentication
+from rest_framework import generics
+
 from django.shortcuts import render
 
-# Create your views here.
+from areas.models import Area
+
+class AreaView(generics.ListCreateAPIView):
+	authentication_classes = [TokenAuthentication]
+
+	queryset = Area.objects.all()
+	serializer_class = 
