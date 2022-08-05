11d4eacc0988c3cea10273c658e16e5a6cc36b28
Christian <christian.mf22@gmail.com>
2022-08-05 17:04:38 -0300

    feat: Create moviments model

--- movements/models.py
+++ movements/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from uuid import uuid4
 
-# Create your models here.
+
+class Movement(models.Model):
+    id = models.UUIDField(primary_key=True, editable=False, default=uuid4)
+    days = models.IntegerField()
+    start_date = models.DateField()
+    end_date = models.DateField()
